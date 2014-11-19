6411f95c6f1c31a0782f4e78b3b67b3bdbd4c1df
Stephen Giles <s.giles@hotmail.co.uk>
2014-11-19 14:31:42 +0000

    0.0.8. Changed file paths for creating files and dirs as cd command not working.

--- lib/make_me_a_gem_called.rb
+++ lib/make_me_a_gem_called.rb
@@ -13,23 +13,23 @@ class MakeMeAGemCalled
     system("bundle gem #{@name}")
   end
 
-  def change_into_directory
-    system("cd #{@name}")
-  end
+  # def change_into_directory
+  #   system("cd #{@name}")
+  # end
 
   def create_spec_files
-    system("mkdir spec")
-    system("touch spec/spec_helper.rb")
-    system("touch spec/#{@name}_spec.rb")
+    system("mkdir #{@name}/spec")
+    system("touch #{@name}/spec/spec_helper.rb")
+    system("touch #{@name}/spec/#{@name}_spec.rb")
   end
 
   def add_to_spec_files
-    system("echo require 'spec_helper' >> spec/#{@name}_spec.rb")
-    system("echo require '#{@name}_spec' >> spec/spec_helper.rb")
+    system("echo require 'spec_helper' >> #{@name}/spec/#{@name}_spec.rb")
+    system("echo require '#{@name}_spec' >> #{@name}/spec/spec_helper.rb")
   end
 
   def add_require_relative_to_main_file
-    system("echo require_relative '#{@name}/version' >> lib/#{@name}.rb")
+    system("echo require_relative '#{@name}/version' >> #{@name}/lib/#{@name}.rb")
   end
 
   def command_line_question
@@ -51,28 +51,28 @@ class MakeMeAGemCalled
   end
 
   def create_bin_files
-    system("mkdir bin")
-    system("touch bin/#{@name}")
+    system("mkdir #{@name}/bin")
+    system("touch #{@name}/bin/#{@name}")
   end
 
   def add_to_bin_files
-    system("echo #!/usr/bin/env ruby >> bin/#{@name}")
-    system("echo require '#{@name}' >> bin/#{@name}")
+    system("echo #!/usr/bin/env ruby >> #{@name}/bin/#{@name}")
+    system("echo require '#{@name}' >> #{@name}/bin/#{@name}")
   end
 
   def create_tasks_files
-    system("mkdir tasks")
-    system("touch tasks/rspec.rake")
+    system("mkdir #{@name}/tasks")
+    system("touch #{@name}/tasks/rspec.rake")
   end
 
   def add_to_tasks_files
-    system("echo require 'rspec/core/rake_task' >> tasks/rspec.rake")
-    system("echo RSpec::Core::RakeTask.new(:spec) >> tasks/rspec.rake")
+    system("echo require 'rspec/core/rake_task' >> #{@name}/tasks/rspec.rake")
+    system("echo RSpec::Core::RakeTask.new(:spec) >> #{@name}/tasks/rspec.rake")
 
   end
 
   def add_to_rake_file
-    system("echo Dir.glob('tasks/**/*.rake').each(&method(:import)) >> Rakefile")
+    system("echo Dir.glob('tasks/**/*.rake').each(&method(:import)) >> #{@name}/Rakefile")
   end
 
   def instructions
