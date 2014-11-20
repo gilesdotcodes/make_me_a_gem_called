d2a2e76b11891e24f2ad7c53a4ed9fe8ca826c42
Stephen Giles <s.giles@hotmail.co.uk>
2014-11-20 11:56:48 +0000

    0.0.25. Could this be the spaces sorted?

--- lib/make_me_a_gem_called.rb
+++ lib/make_me_a_gem_called.rb
@@ -74,7 +74,7 @@ class MakeMeAGemCalled
 
   def add_to_gemspec
     system("sed -i \'\' \'23i\\
-      \'\t\'\'\t\'spec.add_development_dependency \"rspec\"\\
+      \ \ spec.add_development_dependency \"rspec\"\\
       \' #{name}/#{name}.gemspec")
   end
 
